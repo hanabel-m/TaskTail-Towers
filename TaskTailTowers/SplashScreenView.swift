03f3d879240948176ea20f34c99c771bce27d6b4
hanabel-m <hmengistu@umass.edu>
2023-12-28 13:43:33 +0100

    SplashScreen done and imported icons.

---
+++ TaskTailTowers/SplashScreenView.swift
@@ -0,0 +1,18 @@
+//
+//  SplashScreenView.swift
+//  TaskTailTowers
+//
+//  Created by Hanabel Mengistu on 12/27/23.
+//
+
+import SwiftUI
+
+struct SplashScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashScreenView()
+}
